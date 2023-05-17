1ebe644505075ea65502d6b25b654e9fbed429b2
Joshua Lawson <Joshua.lawson@sjsu.edu>
2023-05-16 19:07:14 -0700

    init

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import Sidebar from './components/sidebar/Sidebar';
+import LandingPage from './components/LandingPage';
+
+function App() {
+  return (
+    <>
+      <Sidebar />
+      <LandingPage />
+    </>
+  );
+}
+
+export default App;
